09bd165e0ee11301bde7decb847d0bf9b53b3102
darsh <darsh@DESKTOP-DL374AP>
2022-05-09 20:00:47 +0530

    Commit my changes

--- RedBus/src/main/java/com/redbus/controller/UserController.java
+++ RedBus/src/main/java/com/redbus/controller/UserController.java
@@ -1,5 +1,16 @@
 package com.redbus.controller;
 
 public class UserController {
+	
+	public void m1() {
+		
+		System.out.println("Demo");
+		System.out.println("Demo");
+		System.out.println("Demo");
+		System.out.println("Demo");
+		System.out.println("Demo");
+		System.out.println("Demo");
+	
+	}
 
 }
